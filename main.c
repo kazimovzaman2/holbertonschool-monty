437e419e2e5a1cf49fb3bd05eb9adc1bc8aa03fd
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-03 17:22:33 +0400

    Create main.c for intialize app

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * @argc: var
+ * @argv: var
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+    int file;
+
+    if (argc != 2)
+    {
+        printf("USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = open(argv[1], O_RDONLY);
+    if (file == -1)
+    {
+        printf("Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+}
