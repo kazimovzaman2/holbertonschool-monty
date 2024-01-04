d132bc417b667cf743cde2e181fe2aea021438bb
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-04 01:42:58 -0800

    Betyy git push

--- pint.c
+++ pint.c
@@ -10,10 +10,10 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-    if (!stack)
-    {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
+	if (!stack)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
