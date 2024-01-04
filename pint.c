5acac519c05afc22b332078b9213825fed8cbc7a
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-04 13:53:22 +0400

    Additional if statement

--- pint.c
+++ pint.c
@@ -9,7 +9,7 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if (!stack)
+	if (!stack || !(*stack))
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
