6ec2a2257f38d4db23c05a8a2741f633c6912606
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-04 16:56:40 +0400

    Add new lie

--- add.c
+++ add.c
@@ -13,7 +13,7 @@ void add(stack_t **stack, unsigned int line_number)
 
     if (!(*stack) || !(*stack)->next)
     {
-        fprintf(stderr, "L%d: can't add, stack too short", line_number);
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
         curr = *stack;
 		while (curr != NULL)
 		{
