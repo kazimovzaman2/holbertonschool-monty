a7dc892cde8817ffd4881da5ae726370b72afa08
seyyar95 <heydrovsyyr@gmail.com>
2024-01-08 22:45:56 -0800

    free function added

---
+++ free_stack.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * free_stack - a function that frees all dynamically
+ * allocated memory associated with a stack.
+ *
+ * @stack: pointer to the head of the stack
+ *
+ */
+
+void free_stack(stack_t *stack)
+{
+	stack_t curr, temp;
+
+	curr = stack;
+
+	while (curr != NULL)
+	{
+		temp = curr;
+		curr = curr->next;
+		free(temp)
+	}
+	stack = NULL;
+}
