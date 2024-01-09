47e955f956471aad7655e323aff6b278f5d64c4d
seyyar95 <heydrovsyyr@gmail.com>
2024-01-08 23:19:54 -0800

    Header file updated

--- monty.h
+++ monty.h
@@ -41,7 +41,6 @@ typedef struct instruction_s
 
 void run_instruction(char *line, stack_t **stack,
 instruction_t *instructions, unsigned int line_number);
-void free_stack(stack_t *stack);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
@@ -49,6 +48,7 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
 int is_number(char *arg);
 
 #endif
