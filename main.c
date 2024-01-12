8d11b1c1b7eb9b793947242d8398f39cdd8eccbb
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-12 05:46:17 +0400

    Sub

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	instruction_t instructions[] = {
 		{"push", push}, {"pall", pall}, {"pint", pint},
 		{"pop", pop}, {"swap", swap}, {"add", add},
-		{"nop", nop}, {NULL, NULL}
+		{"nop", nop}, {"sub", sub}, {NULL, NULL}
 	};
 
 	if (argc != 2)
---
+++ sub.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+
+/**
+ * sub - subtracts the top element of the stack from the second top element
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number being executed from script file
+ *
+ * Return: void
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	int sub = 0;
+
+	if (!temp || !temp->next)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	sub = temp->next->n - temp->n;
+	pop(stack, line_number);
+	(*stack)->n = sub;
+}
