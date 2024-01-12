6b74a7ba356ae01a23fd0ec591a6775f9e7171e3
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-12 20:08:52 +0400

    Change error message

--- mod.c
+++ mod.c
@@ -15,7 +15,7 @@ void mod(stack_t **stack, unsigned int line_number)
 
 	if (!stack || !*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
