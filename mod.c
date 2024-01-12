42a81742bf0a7f6047e92a0e57893b124b88516d
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-12 20:10:44 +0400

    Division 0 error

--- mod.c
+++ mod.c
@@ -18,6 +18,11 @@ void mod(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 	num = (*stack)->next->n % (*stack)->n;
 	pop(stack, line_number);
