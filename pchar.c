bc045d78209e30a02672ba432ca30f6a41c7f453
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-13 08:06:47 +0400

    unuserd temp

--- pchar.c
+++ pchar.c
@@ -10,17 +10,15 @@
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
-
-	if (temp == NULL)
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if (temp->n < 0 || temp->n > 127)
+	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%c\n", temp->n);
+	printf("%c\n", (*stack)->n);
 }
