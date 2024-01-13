e4f2bdfa4e3eeaa86ec6a6e8f9200cb2f0d6867b
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-13 08:31:15 +0400

    Advanced: Pstr

---
+++ pstr.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+
+
+/**
+ * pstr_recursive - helper function for pstr
+ * @node: pointer to the current node
+ *
+ * Return: void
+ */
+void pstr_recursive(stack_t *node)
+{
+	if (!node || node->n == 0)
+		return;
+
+	putchar(node->n);
+	pstr_recursive(node->next);
+}
+
+/**
+ * pstr - prints the string starting at the top of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number of the opcode
+ *
+ * Return: void
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	(void) line_number;
+	if (stack == NULL || *stack == NULL)
+		return;
+
+	pstr_recursive(*stack);
+	putchar('\n');
+}
