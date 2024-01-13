133afd70d49822ed16e26ddd2052db17820e7aee
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-13 08:40:57 +0400

    PSTR

--- pstr.c
+++ pstr.c
@@ -1,22 +1,6 @@
 #include "monty.h"
 
 
-
-/**
- * pstr_recursive - helper function for pstr
- * @node: pointer to the current node
- *
- * Return: void
- */
-void pstr_recursive(stack_t *node)
-{
-	if (!node || node->n == 0)
-		return;
-
-	putchar(node->n);
-	pstr_recursive(node->next);
-}
-
 /**
  * pstr - prints the string starting at the top of the stack
  * @stack: pointer to the top of the stack
@@ -26,11 +10,25 @@ void pstr_recursive(stack_t *node)
  */
 void pstr(stack_t **stack, unsigned int line_number)
 {
-	(void) line_number;
-	if (stack == NULL || *stack == NULL)
-		return;
+	stack_t *temp;
+	int num;
 
-	pstr_recursive(*stack);
-	if ((*stack)->n != 0)
+	(void) line_number;
+
+	if (!(*stack))
+	{
 		putchar('\n');
+		return;
+	}
+	temp = *stack;
+	while (temp)
+	{
+		num = temp->n;
+
+		if (num <= 0 || num > 127)
+			break;
+		putchar(num);
+		temp = temp->next;
+	}
+	putchar('\n');
 }
